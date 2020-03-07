91a6b1dfe20a126074512481044853792d38be6e
Harsh Patel <harshaudi27@gmail.com>
2020-03-07 13:01:24 +0530

    Removed admin panel

--- admin/src/routes/index.jsx
+++
@@ -1,5 +0,0 @@
-import App from "containers/App/App.jsx";
-
-const indexRoutes = [{ path: "/", component: App }];
-
-export default indexRoutes;
--- src/containers/App/AppMain.jsx
+++ src/containers/App/AppMain.jsx
@@ -64,7 +64,7 @@ class AppMain extends React.Component {
             handleDrawerToggle={this.handleDrawerToggle}
             {...rest}
           />
-          {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
+
           {this.getRoute() ? (
             <div className={classes.content}>
               <div className={classes.container}>{switchRoutes}</div>
