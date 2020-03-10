abf94ac97b0187932610e89e0c818bfef74e0cf8
Harsh Patel <harshaudi27@gmail.com>
2020-03-10 17:36:14 +0530

    Added cryptos list along with basic metadata fetch api

--- api/app.py
+++ api/app.py
@@ -1,12 +1,24 @@
 import time
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
+import urllib
+import os
+from dotenv import load_dotenv
+
+# Load secret API file
+load_dotenv()
+API_KEY = os.getenv('NOMICS_API_KEY')
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return jsonify({'time': time.time()})
+
+@app.route('/crypto/metadata/<tick>')
+def getCryptoMetaData(tick):
+	url = "https://api.nomics.com/v1/currencies?key=" + API_KEY + "&ids=" + tick
+	response = urllib.urlopen(url).read()
+	return response
 
 if __name__ == '__main__':
     app.run()
