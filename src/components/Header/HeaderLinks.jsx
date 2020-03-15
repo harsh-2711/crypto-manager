0ac72c14a141d52d7cd6b8eb7328662106865d47
jay-mehta531 <62137920+jay-mehta531@users.noreply.github.com>
2020-03-15 19:37:34 +0530

    Fix Notification Issue (#7)

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -28,6 +28,10 @@ class HeaderLinks extends React.Component {
   handleClose = () => {
     this.setState({ open: false });
   };
+
+  handleCloseNone = () => {
+
+  };
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -61,78 +65,79 @@ class HeaderLinks extends React.Component {
             <p className={classes.linkText}>Dashboard</p>
           </Hidden>
         </IconButton> */}
-        <Manager style={{ display: "inline-block" }}>
-          <Target>
-            <IconButton
-              color="inherit"
-              aria-label="Notifications"
-              aria-owns={open ? "menu-list" : null}
-              aria-haspopup="true"
-              onClick={this.handleClick}
-              className={classes.buttonLink}
-            >
-              <Notifications className={classes.links} />
-              <span className={classes.notifications}>5</span>
-              <Hidden mdUp>
-                <p onClick={this.handleClick} className={classes.linkText}>
-                  Notification
-                </p>
-              </Hidden>
-            </IconButton>
-          </Target>
-          <Popper
-            placement="bottom-start"
-            eventsEnabled={open}
-            className={
-              classNames({ [classes.popperClose]: !open }) +
-              " " +
-              classes.pooperResponsive
-            }
-          >
-            <ClickAwayListener onClickAway={this.handleClose}>
-              <Grow
-                in={open}
-                id="menu-list"
-                style={{ transformOrigin: "0 0 0" }}
+
+        <ClickAwayListener  onClickAway={open?this.handleClick:this.handleCloseNone}>
+          <Manager style={{ display: "inline-block" }}>
+            <Target>
+              <IconButton
+                color="inherit"
+                aria-label="Notifications"
+                aria-owns={open ? "menu-list" : null}
+                aria-haspopup="true"
+                onClick={this.handleClick}
+                className={classes.buttonLink}
               >
-                <Paper className={classes.dropdown}>
-                  <MenuList role="menu">
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={classes.dropdownItem}
-                    >
-                      Mike John responded to your email
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={classes.dropdownItem}
-                    >
-                      You have 5 new tasks
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={classes.dropdownItem}
-                    >
-                      You're now friend with Andrew
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={classes.dropdownItem}
-                    >
-                      Another Notification
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={classes.dropdownItem}
-                    >
-                      Another One
-                    </MenuItem>
-                  </MenuList>
-                </Paper>
-              </Grow>
-            </ClickAwayListener>
-          </Popper>
-        </Manager> 
+                <Notifications className={classes.links} />
+                <span className={classes.notifications}>5</span>
+                <Hidden mdUp>
+                  <p onClick={this.handleClick} className={classes.linkText}>
+                    Notification
+                  </p>
+                </Hidden>
+              </IconButton>
+            </Target>
+            <Popper
+              placement="bottom-start"
+              eventsEnabled={open}
+              className={
+                classNames({ [classes.popperClose]: !open }) +
+                " " +
+                classes.pooperResponsive
+              }
+            >
+                <Grow
+                  in={open}
+                  id="menu-list"
+                  style={{ transformOrigin: "0 0 0" }}
+                >
+                  <Paper className={classes.dropdown}>
+                    <MenuList role="menu">
+                      <MenuItem
+                        onClick={this.handleClose}
+                        className={classes.dropdownItem}
+                      >
+                        Mike John responded to your email
+                      </MenuItem>
+                      <MenuItem
+                        onClick={this.handleClose}
+                        className={classes.dropdownItem}
+                      >
+                        You have 5 new tasks
+                      </MenuItem>
+                      <MenuItem
+                        onClick={this.handleClose}
+                        className={classes.dropdownItem}
+                      >
+                        You're now friend with Andrew
+                      </MenuItem>
+                      <MenuItem
+                        onClick={this.handleClose}
+                        className={classes.dropdownItem}
+                      >
+                        Another Notification
+                      </MenuItem>
+                      <MenuItem
+                        onClick={this.handleClose}
+                        className={classes.dropdownItem}
+                      >
+                        Another One
+                      </MenuItem>
+                    </MenuList>
+                  </Paper>
+                </Grow>
+            </Popper>
+          </Manager> 
+        </ClickAwayListener>
         <IconButton
           color="inherit"
           aria-label="Person"
