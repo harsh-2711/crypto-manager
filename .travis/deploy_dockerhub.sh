a416d7fa33a858912473dcd5a0beb6d2ade3cb2c
Harsh Patel <harshaudi27@gmail.com>
2020-04-05 03:59:24 +0530

    Modified docker deploy shell script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ else
 TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
-docker push $DOCKER_REPO
+docker tag $TRAVIS_REPO_SLUG:$TAG $DOCKER_USER/$DOCKER_REPO
+docker push $DOCKER_USER/$DOCKER_REPO
