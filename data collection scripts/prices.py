b5a646f4f4bec0dff3efa15ea479ecbf2411b6dd
nikshah4120 <50862248+nikshah4120@users.noreply.github.com>
2020-04-05 16:42:05 +0530

    Update prices.py

--- data collection scripts/prices.py
+++ data collection scripts/prices.py
@@ -1,28 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  4 09:34:03 2020
-
-@author:Nikunj Shah
-"""
-
-import tweepy
-import csv
+#Storing Date,High price,Low price,Open Price,Close Price  of 10 different crypto currencies for last 8 days
+import requests
+from datetime import datetime
 import pandas as pd
-import time
+to_symbol = 'USD'
+datetime_interval = 'day'
 
 
 
-####input your credentials here
-consumer_key = 'vgnSRlDDLhMHdukzJhlWAaPXq'
-consumer_secret = 'ryHjEr6fgwM4wf263qe6eEegwrYcWIrBhGoqEHNihhGzPjFq0W'
-access_token = '794799425587650560-VBN8MEyzFYhEGGByEeGnfnbfS8NwMow'
-access_token_secret = 'mq5Fdr8vWsi2QuCyFWDVE7cDGwm8nWKf3TYV68GMxCgJR'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True)
 
-list=['bitcoin','ETH','XRP','LTC','BCH','Monero','TRON','Zcash','Binance coin','Huobi token'];
+
 #index cryptocurrencies
 # 1    bitcoin
 # 2    Ethereum
@@ -34,15 +21,55 @@ list=['bitcoin','ETH','XRP','LTC','BCH','Monero','TRON','Zcash','Binance coin','
 #8     Zcash
 #9    Binance coin
 #10   Huobi token
-# Open/Create a file to append data
-csvFile =open('tweet_data1.csv', 'a')
-#Use csv Writer
-c=0
-csvWriter = csv.writer(csvFile,lineterminator='\n')
-csvWriter.writerow(['index','username','tweet','likes_count','retweet_count','date & time']);
-for i in range(len(list)):
-        s=list[i]
-        print(i)
-        for tweet in tweepy.Cursor(api.search,q=s,lang="en",count=1000,since="2020-03-29").items():
-                print(tweet.created_at)
-                csvWriter.writerow([i+1,tweet.user.screen_name,tweet.text.encode('utf-8'),tweet.favorite_count,tweet.retweet_count,tweet.created_at]);
+  
+crypto_list=['BTC','ETH','XRP','LTC','BCH','XMR','TRX','ZEC','BNB','HT']
+
+
+def download_data(from_symbol, to_symbol, exchange, datetime_interval):
+    supported_intervals = {'minute', 'hour', 'day'}
+    assert datetime_interval in supported_intervals,\
+        'datetime_interval should be one of %s' % supported_intervals
+    print('Downloading %s trading data for %s %s from %s' %
+          (datetime_interval, from_symbol, to_symbol, exchange))
+    base_url = 'https://min-api.cryptocompare.com/data/histo'
+    url = '%s%s' % (base_url, datetime_interval)
+    params = {'fsym': from_symbol, 'tsym': to_symbol,
+              'limit': 2000, 'aggregate': 1,
+              'e': exchange}
+    request = requests.get(url, params=params)
+    data = request.json()
+    return data
+
+def convert_to_dataframe(data):
+    df = pd.io.json.json_normalize(data, ['Data'])
+    df['datetime'] = pd.to_datetime(df.time, unit='s')
+    df = df[['datetime', 'low', 'high', 'open',
+             'close', 'volumefrom', 'volumeto']]
+    return df
+def filter_empty_datapoints(df):
+    indices = df[df.sum(axis=1) == 0].index
+    print('Filtering %d empty datapoints' % indices.shape[0])
+    df = df.drop(indices)
+    return df
+
+
+#main code
+for k in range(len(crypto_list)):
+    from_symbol=crypto_list[k]
+    if(from_symbol=='BNB'):
+        exchange='coinsbit'
+    elif(from_symbol=='HT'):
+        exchange='HitBTC'
+    else:
+        exchange='bitfinex'
+    data = download_data(from_symbol, to_symbol, exchange, datetime_interval)
+    df = convert_to_dataframe(data)
+    df = filter_empty_datapoints(df)
+    df.insert(0,'index',k+1)
+    if(k==0): 
+      m='w'
+      h=True
+    else:
+      m='a'
+      h=False
+    df.tail(8).to_csv('price.csv',mode=m,index=False,header=h)
