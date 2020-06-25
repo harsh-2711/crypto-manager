b0d6d8956ae9adef9a897159da7f31692a3dd444
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-25 15:16:03 +0000

    Deploy harsh-2711/crypto-manager to github.com/harsh-2711/crypto-manager.git:gh-pages

---
+++ precache-manifest.e8d2d1fb58b167b1e4251a29f43b1a56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac30a9e4f2d56f136f9895c901aeee8",
+    "url": "/crypto-manager/index.html"
+  },
+  {
+    "revision": "faa3737f3cbdd0bf8008",
+    "url": "/crypto-manager/static/css/2.bfa6f0c0.chunk.css"
+  },
+  {
+    "revision": "c4a8e1473edae83a335e",
+    "url": "/crypto-manager/static/css/main.c6eaa3a0.chunk.css"
+  },
+  {
+    "revision": "faa3737f3cbdd0bf8008",
+    "url": "/crypto-manager/static/js/2.08d9ff52.chunk.js"
+  },
+  {
+    "revision": "248bd1f2108b459e83c1b896c9c8facd",
+    "url": "/crypto-manager/static/js/2.08d9ff52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a8e1473edae83a335e",
+    "url": "/crypto-manager/static/js/main.db22b910.chunk.js"
+  },
+  {
+    "revision": "33e1416c9cbb8e4add0d",
+    "url": "/crypto-manager/static/js/runtime-main.3433c71d.js"
+  },
+  {
+    "revision": "fe9bc614df69ea3b7b65f73b0952d205",
+    "url": "/crypto-manager/static/media/bitcoin.fe9bc614.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/crypto-manager/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/crypto-manager/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
